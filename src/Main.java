47b712d27bb09bff35a0b51091a91228facdb417
Mohammad Ghnim <mohammadghnim92@gmail.com>
2025-12-08 10:34:18 +0100

    ester commit

---
+++ src/Main.java
@@ -0,0 +1,103 @@
+import java.sql.SQLOutput;
+
+/*
+    public class Main {
+        public static void main(String[] args){
+
+            // This is my first Java program (It's only a comment)
+
+            /*
+                This
+                is
+                a
+                multi-line
+                comment
+                smile 😂 (Windows taste + . )
+
+
+            System.out.println("I like pizza!");
+            System.out.println("It's really good!");
+            System.out.println("Buy me pizza!");
+            System.out.println("This is a smile 😂");
+
+            Mit Boolean:
+            if(isStudent){
+                System.out.println("You are a student!");
+            }
+            else{
+                System.out.println("You are NOT a student!");
+            }
+
+            System.out.println("Hallo " + name);
+            System.out.println("You ara " + age + " years old");
+            System.out.println("Your gpa is: " + gpa);
+            System.out.println("Your average letter grad is: " + grade);
+
+            System.out.println("Your choice is a " + color + " " + year + " " + car);
+            System.out.println("The price is: " + currency + price);
+
+            if(forSale){
+                System.out.println("There is a " + car + " for sale. ");
+            }
+            else{
+                System.out.println("The " + car + " is not for sale.");
+            }
+
+
+
+            int age = 21;
+            int year = 2025;
+            int quantity = 1;
+
+            double price = 19999.99;
+            double gpa = 3.5;
+            double temperature = -12.5;
+
+            char grade = 'A';
+            char symbol = '!';
+            char currency = '$';
+
+
+            boolean isStudent = false;
+            boolean forSale = true;
+            boolean isOnline = true;
+
+
+            String name = "Hakuna Matata";
+            String food = "pizza";
+            String email = "hakunamatata92@gmail.com";
+            String car = "Mustang";
+            String color = "red";
+
+            // andere Beispiele von Jonas:
+            int number = 15;
+            int number2 = 20;
+            int sum = number + number2;
+
+            int apples= 12;
+
+            int anzahl = 20;
+            String obst = " Birnen";
+
+            String text = "Ich habe so viele Äpfel: ";
+
+
+            System.out.println("Ich möchte " + anzahl + obst);
+        }
+    }
+*/
+/*
+public class Main{
+    public static void main(String[] args){
+        String Name="Hakuna Matata";
+        System.out.println("Hallo " + Name);
+    }
+}
+*/
+
+public class Main (){
+    public static void main(String[] args){
+        System.out.print();
+    }
+}
+
